cc32111496fdcf17b1829e5d84038e6df27287d0
Evgeniy Milyakov <milyakove@gmail.com>
2022-09-27 22:39:26 +0300

    fixed component ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 function ProtectedRoute({ component: Component, ...props }) {
-  return <Route>{() => (props.loggedIn ? <Component {...props} /> : <Redirect to="/sign-in" />)}</Route>;
+  return (<Route> 
+    {
+      () => props.loggedIn ? <Component {...props} /> : <Redirect to="/sign-in" />
+    }
+  </Route>);
 }
 
 export default ProtectedRoute;
